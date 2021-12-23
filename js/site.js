0bc825ab3b7f0f5965cdc091c641afbeae760fbe
Kevin MacKenzie <kevin@homesmoncton.com>
2021-12-23 18:57:59 -0400

    Completed js code for the app

--- js/site.js
+++ js/site.js
@@ -1,5 +1,23 @@
-// call helloWorld
+// get the input from form
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+};
 
-function helloWorld {
-    alert(`Hello World`);
-}
+// reverse string
+function reverseString(userString) {
+    let revString = [];
+    // reverse string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+    return revString;
+};
+
+// display the result
+function displayString(revstring) {
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revstring}`;
+    document.getElementById("alert").classList.remove("invisible");
+};
